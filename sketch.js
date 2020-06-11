1e4b76a2b54b824c5ca68a79bbc0889dcd4d44a7
Surya Konduru <66318719+surya-konduru@users.noreply.github.com>
2020-06-11 21:01:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var fixedrect, movingrect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedrect = createSprite(400, 100, 50, 80);
+  fixedrect.shapeColor = "yellow";
+  fixedrect.debug = true;
+  movingrect = createSprite(400, 800, 80, 30);
+  movingrect.shapeColor = "yellow";
+  movingrect.debug = true;
+  
+  movingrect.velocityY = -5;
+  fixedrect.velocityY = 5;
+}
+
+function draw() {
+  background(0);
+
+  bounceOff(movingrect, fixedrect);
+  drawSprites();
+}
+
+function bounceOff(object1, object2){
+  if(object1.x - object2.x < object2.width/2 + object1.width/2 && 
+    object2.x - object1.x < object2.width/2 + object1.width/2 ){
+     object2.velocityX  = object2.velocityX * (-1);
+     object1.velocityX = object1.velocityX * (-1);
+ 
+   }
+   if( object1.y - object2.y < object2.height/2 + object1.height/2 && 
+     object2.y - object1.y < object2.height/2 + object1.height/2){
+       object2.velocityY  = object2.velocityY * (-1);
+       object1.velocityY = object1.velocityY * (-1);
+     }
+}
